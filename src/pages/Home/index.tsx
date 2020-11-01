500f60f19a5bba160a4e23e25802db2de4b27ae2
Vinicius Flores <viniciusflores379@gmail.com>
2020-11-01 18:20:46 -0300

    :beers: test request fail - roolback

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -33,9 +33,11 @@ const Home: React.FC = () => {
     }
 
     try {
-      const response = await api.get(`/pokemon/${pokemonQuery}`).then((res) => {
-        return res.data;
-      });
+      const response = await api
+        .get(`https://pokeapi.co/api/v2/pokemon/${pokemonQuery}`)
+        .then((res) => {
+          return res.data;
+        });
 
       console.log(response);
 
